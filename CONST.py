d8a64f5376bff0379fda5297029db1ba29df2a18
Maja Kruszewska <majakruszewska@MacBook-Air-Maja.local>
2026-01-05 21:51:45 +0100

    Poprawa wielkości ekranu

--- CONST.py
+++ CONST.py
@@ -1,6 +1,6 @@
 # Wymiary ekranu
-HEIGHT = 810 
-WIDTH = 1170
+HEIGHT = 920
+WIDTH = 1230
 # Wielkości kafelków (Ekran // dlugość tablicy)
 TILE_SIZE_X = WIDTH  // 41 # szerokość
 TILE_SIZE_Y = (HEIGHT - 50)// 29 # wysokość
