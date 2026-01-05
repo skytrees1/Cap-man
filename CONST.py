1712ffec12252a04af8028abef261cb867ae4358
Maciej Jerzycki <361726@uwr.edu.pl>
2026-01-05 22:57:25 +0100

    Poprawa miejsca startowego na poprawionej mapie

--- CONST.py
+++ CONST.py
@@ -1,11 +1,11 @@
 from board import board
-HEIGHT = 810
-WIDTH = 1170
+HEIGHT = 920
+WIDTH = 1230
 TILE_SIZE_X = WIDTH  // 41
 TILE_SIZE_Y = (HEIGHT - 50)// 29
 #Wysokość grywalna (ilość kafelek) planszy - 4 kafelki, 2 na obrzeża góry i dołu
 BOARD_HEIGHT = len(board) - 4
 DIRECTION = None
 #Początkowa pozycja Cap-Mana na mapie
-STARTING_POSITION_X = 572
-STARTING_POSITION_Y = 612
+STARTING_POSITION_X = WIDTH - (TILE_SIZE_X * 20) - 15
+STARTING_POSITION_Y = HEIGHT - 7 * TILE_SIZE_Y  - 4
